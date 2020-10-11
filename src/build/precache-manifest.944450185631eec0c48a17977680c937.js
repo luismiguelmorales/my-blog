52262879a4d5a18edd59d93705f968fc35559c78
Luis Miguel Morales <luis.17.miguel.7@gmail.com>
2020-10-11 18:00:46 -0500

    First commit

---
+++ src/build/precache-manifest.944450185631eec0c48a17977680c937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1912453828801030e341df7b07d7ec86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0a41726f838cd406bb9",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a41726f838cd406bb9",
+    "url": "/static/js/main.c4efa7d5.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
